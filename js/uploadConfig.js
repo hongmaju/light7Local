d7055fe13ed39fe1d1b7ff82a2a5e7b45cf5b6b4
HongMaJu <517412663@qq.com>
2017-05-18 15:15:28 +0800

    上传的前端

---
+++ js/uploadConfig.js
@@ -0,0 +1,57 @@
+﻿
+var uploadConfig = {
+    removeImg: function (r) {//描述：上传图片附带删除 再次地方可以加上一个ajax进行提交到后台进行删除
+        var url = $(r).attr("imgsrc");
+        if (url.length > 0) {
+            $.ajax({
+                url: '../Controller/deleteImg.ashx',
+                type: 'post',
+                cache: false,
+                dataType: 'text',
+                data: {
+                    imgUrl: $(r).attr("imgsrc")
+
+                },
+                success: function (data) {
+                    alert(data);
+
+                }
+            });
+
+        }
+        
+        $(r).parent().remove();
+    },
+    hotelInfo: function () {
+        var html = ue.getContent();
+        
+        
+        html = html.replace(/[\r\n]/g, "");
+        //html = html.replace(/\%/g, "%25");
+        //html = html.replace(/\+/g, "%2B");
+        //html = html.replace(/\&/g, "%26");
+        if (html.length > 0) {
+            $.ajax({
+                url: '../Controller/updateHotelInfo.ashx',
+                type: 'post',
+                cache: false,
+                async: false,
+                dataType: 'text',
+                data: {
+                    //conten: html
+                    conten: encodeURIComponent(html)
+                    
+                    //data : { "parametersStr":encodeURIComponent(queryxml),"lotime":encodeURIComponent(new Date())}, 
+
+                },
+                success: function (data) {
+                    alert(data);
+
+                }
+            });
+
+        }
+    }
+
+
+};
